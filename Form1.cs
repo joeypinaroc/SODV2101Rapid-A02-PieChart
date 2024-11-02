b01ac258f9087a5211c1d94c946117f5bb4433c0
gperez314 <g.perez314@mybvc.ca>
2024-11-02 13:45:38 -0600

    UI interlock to handle if all colors are used

--- Form1.cs
+++ Form1.cs
@@ -16,13 +16,21 @@ namespace PieChart
         public int i = 0; //index for colors List
         public static List<Color> colors = new List<Color>() //static list for colors
         {
-            Color.Black,
             Color.Blue,
             Color.Green,
-            Color.Red,
             Color.Yellow,
+            Color.Red,
+            Color.Violet,
+            Color.Orange,
             Color.GreenYellow,
-            Color.DarkRed
+            Color.PaleVioletRed,
+            Color.BlueViolet,
+            Color.Cyan,
+            Color.DarkRed,
+            Color.Magenta,
+            Color.OrangeRed,
+            Color.Teal,
+            Color.Brown
         };
         public List<float> numbers = new List<float>();
         public static List<float> ConvertToDegrees(List<float> values) //convert input list to a list of degrees summing to 360
@@ -45,7 +53,13 @@ namespace PieChart
             Graphics graphics = this.CreateGraphics();
             Pen outline = new Pen(Color.Black, 3);
 
-            Rectangle rect = new Rectangle(450, 250, 400, 400);
+            Rectangle rect = new Rectangle(450, 200, 400, 400);
+
+            // Draw the legend
+            int legendX = 880;
+            int legendY = 200;
+            int legendWidth = 20;
+            int legendHeight = 20;
 
             List<float> degrees = ConvertToDegrees(numbers); //convert input numbers list to a list of degrees
 
@@ -56,23 +70,13 @@ namespace PieChart
                 e.Graphics.DrawPie(outline, rect, startAngle, degrees[i]);
                 e.Graphics.FillPie(new SolidBrush(colors[i]), rect, startAngle, degrees[i]);
                 startAngle += degrees[i];
-            }
 
-            // Draw the legend
-            int legendX = 880;
-            int legendY = 250;
-            int legendWidth = 20;
-            int legendHeight = 20;
-
-            // Loop through to create legend entries
-            for (int i = 0; i < numbers.Count; i++)
-            {
                 // Draw the color box for the legend
                 Brush colorBrush = new SolidBrush(colors[i]);
-                graphics.FillRectangle(colorBrush, legendX, legendY + (i * (legendHeight + 5)), legendWidth, legendHeight);
+                e.Graphics.FillRectangle(colorBrush, legendX, legendY + (i * (legendHeight + 5)), legendWidth, legendHeight);
 
                 // Draw the corresponding label
-                graphics.DrawString(numbers[i].ToString() + " - " + colors[i].Name,
+                e.Graphics.DrawString(numbers[i].ToString() + " - " + colors[i].Name,
                     this.Font, Brushes.Black, legendX + legendWidth + 5, legendY + (i * (legendHeight + 5)));
             }
         }
@@ -87,16 +91,22 @@ namespace PieChart
             float temp;
             if ((textBox_Input != null) && (float.TryParse(textBox_Input.Text,out temp)))
             {
-                temp = float.Parse(textBox_Input.Text);
-                listBox.Items.Add(temp + " - " + colors[i].Name);
-                numbers.Add(temp);
-                textBox_Input.Text = "";
-                i++; //index scrolls through colors list
+                if (i < colors.Count)
+                {
+                    listBox.Items.Add(temp + " - " + colors[i].Name);
+                    numbers.Add(temp);
+                    i++; //index scrolls through colors list
+                }
+                else
+                {
+                    MessageBox.Show("Maximum number of entries reached.");
+                }
             }
             else
             {
                 MessageBox.Show("Please enter a valid number.");
             }
+            textBox_Input.Text = ""; // Clear textbox input
         }
 
         private void btn_ResetList_Click(object sender, EventArgs e)
